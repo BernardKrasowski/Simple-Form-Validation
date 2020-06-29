3449353258b48002a8540285529f5cebce2b4411
Bernard Krasowski <bernardkrasowski111@gmail.com>
2020-06-29 12:52:16 +0200

    Connect inp Name & Email with state

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    username: '',
+    email: '',
+  }
 
+  handleChnge = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
   render() {
     return (
 
-      <div></div>
+      <div className="App">
+        <form>
+          <label htmlFor="user">Name:
+          <input type="text" id='user' name="username"
+              value={this.state.username} onChange={this.handleChnge} />
+          </label>
+
+          <label htmlFor="email">Email:
+          <input type="email" id='email' name="email"
+              value={this.state.email} onChange={this.handleChnge} />
+          </label>
+
+          {/* <label htmlFor="user">Your Name:
+          <input type="text" id='user' name="username"
+              value={this.state.username} onChange={this.handleChnge} />
+          </label> */}
+        </form>
+      </div>
 
     );
   }
